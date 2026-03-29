e4c43800e28898d818eb386f0539ff2eb4e4bc0d
Carlos App <107434698+CarlosEApp@users.noreply.github.com>
2026-03-28 21:06:06 -0300

    Add files via upload

--- js/filmes.js
+++ js/filmes.js
@@ -76,3 +76,108 @@ produtosRef.get().then((querySnapshot) => {
 
 }
 filmesSites()
+
+
+//Pesquisa
+function pesquisarProduto() {
+   document.getElementById('respPesquisasadiv').style.display='none'
+  var list = document.getElementById('listpesqRes');
+  list.innerHTML = ''
+  var termo = document.getElementById("pesquise").value.toLowerCase();
+  if(termo){
+  setTimeout(function(){
+ Swal.fire({ 
+title: ``,
+text: ``, 
+html:`
+<div>
+<b id="bbdesc"> Não encontramos nada relacionado as informações digitadas</b><br> digite as primeiras letras  do que deseja encontrar e procure em uma lista a opção desejada<b id="b"></b> .
+      </div> 
+`,
+imageUrl: ``,
+background: '#000303',
+color: '#a7a7a7', // cor do texto });
+showCloseButton: true,   // habilita o "X"
+allowOutsideClick: false,
+showConfirmButton: false,
+customClass: {
+popup: 'my-admin' // Aplica a classe CSS personalizada
+},
+didOpen: () => {
+document.body.style.paddingRight = '0px';   
+}
+})
+ setTimeout(function(){
+Swal.close()
+    },25000)
+ },1000)
+ 
+  var db= firebase.firestore()
+  db.collection("Lista Geral").get().then(snapshot => {
+ 
+    snapshot.forEach(docSnap => {
+      var data = docSnap.data();
+      if (data.Titulo && data.Titulo.toLowerCase().includes(termo) || data.SubTitulo && data.SubTitulo.toLowerCase().includes(termo)  ||data.OBS && data.OBS.toLowerCase().includes(termo) )  {
+        var titulo = data.Titulo ? data.Titulo.toLowerCase() : ""; var sub = data.SubTitulo ? data.SubTitulo.toLowerCase() : ""; var obs = data.OBS ? data.OBS.toLowerCase() : "";
+          
+          if (titulo.includes(termo) || termo.includes(titulo)||obs.includes(termo) || termo.includes(obs)||sub.includes(termo) || termo.includes(sub)) {
+        
+       //alert(data.Titulo)
+  
+        var divbase=document.createElement('div');
+        var div=document.createElement('div');
+        var div2=document.createElement('div');
+        var imagem=document.createElement('img');
+        var label=document.createElement('label');
+        var label2=document.createElement('label');
+        var label3=document.createElement('label');
+        
+        divbase.id='divbase';
+        div.id='divum';
+        div2.id='divdois';
+        imagem.id='imagemps';
+        label.id='lbl1';
+        label2.id='lbl2';
+        label3.id='lbl3';
+
+         imagem.src=data.Imagem;
+         label.textContent= data.Titulo;
+         label2.textContent= data.SubTitulo
+
+         div.appendChild(imagem);
+         div2.appendChild(label);
+         div2.appendChild(label2);
+         divbase.appendChild(div);
+         divbase.appendChild(div2);
+         list.appendChild(divbase)
+          document.getElementById('respPesquisasadiv').style.display='block'
+         setTimeout(function(){
+         Swal.close()
+        },1200)
+           
+        divbase.addEventListener('click',function(){
+          //alert(data.Titulo)
+          if(data.Origem==='site'){
+          window.open(`${data.Links}`,'_blank')
+          } else if(data.Origem==='YouTube'){
+             window.open(`../html/resutP.html`,'_blank')
+          }else{
+           
+          }
+        });
+       }
+      
+      } else{
+      }
+    });
+  });
+}else{
+  Swal.fire('','Preencha o campo de pesquisa!','')
+}
+}
+document.getElementById('lblfchardivPesq').addEventListener('click',function(){
+fech()
+})
+function fech(){
+   document.getElementById('respPesquisasadiv').style.display='none'
+}
