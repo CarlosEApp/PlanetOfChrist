f3eea4344664a3c7b600865400ca6dd2edc9e167
Carlos App <107434698+CarlosEApp@users.noreply.github.com>
2026-04-04 00:15:17 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -647,7 +647,7 @@ if (i == 0){
 i = 1;
 var elem = document.getElementById("myBarr");
 var width = 1;
-var id = setInterval(frame, 55);
+var id = setInterval(frame, 33);
 function frame() {
 if (width >= 100) {
 i = 0;
